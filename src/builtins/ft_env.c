1edc0ece3def0c87515ad8677daa1fb65c9bf894
Axedassis <lssvgabriel@gmail.com>
2025-04-27 18:15:43 -0300

    fix(env): Display valid env

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 21:34:59 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/04/27 16:54:55 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/04/27 18:12:13 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ int		ft_env(void)
 		return (1);
 	while (env[++i])
 	{
-		ft_putstr_fd(env[i], 1);
-		write(1, "\n", 1);
+		if (ft_strchr(env[i], '='))
+		{
+			ft_putstr_fd(env[i], 1);
+			write(1, "\n", 1);
+		}
+		else
+			continue ;
 	}
 	return (0);
 }
