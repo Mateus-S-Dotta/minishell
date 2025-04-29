6fed53ba3269b2e48332b6160549c2355396e9f7
Axedassis <lssvgabriel@gmail.com>
2025-04-28 21:31:47 -0300

    Fix(echo): Undefined behavior

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:49:37 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/04/27 20:21:39 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/04/28 21:31:03 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,23 @@
 int	ft_echo(t_cmds *cmd)
 {
 	int		flag_opt;
-	int		res;
-	t_flags	*flag_tmp;
+	t_flags	*f_tmp;
 
 	flag_opt = 0;
 	if (cmd->flags)
 	{
-		flag_tmp = cmd->flags;
-		while (ft_strncmp(flag_tmp->flag, "-n", 2) == 0)
+		f_tmp = cmd->flags;
+		while (ft_strncmp(f_tmp->flag, "-n", 2) == 0)
 		{
 			flag_opt = 1;
-			flag_tmp = flag_tmp->next;
+			f_tmp = f_tmp->next;
 		}
-		while (flag_tmp)
+		while (f_tmp)
 		{
-			ft_putstr_fd(flag_tmp->flag, 1);
-			res = ft_strncmp(flag_tmp->next->flag, "\0", 1);
-			if (flag_tmp->next && res != 0)
+			ft_putstr_fd(f_tmp->flag, 1);
+			if (f_tmp->next && ft_strncmp(f_tmp->next->flag, "\0", 1) != 0)
 				write(1, " ", 1);
-			flag_tmp = flag_tmp->next;
+			f_tmp = f_tmp->next;
 		}
 	}
 	if (flag_opt == 0)
